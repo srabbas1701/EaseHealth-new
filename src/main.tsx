f00a513677a1873cf45a9c6580b77941caf1112e
rabbas <srabbas1701@gmail.com>
2025-09-22 20:00:06 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
+//import { StrictMode } from 'react';
+//import { createRoot } from 'react-dom/client';
+//import App from './App.tsx';
+//import './index.css';
+//
+//createRoot(document.getElementById('root')!).render(
+//  <StrictMode>
+ //   <App />
+ // </StrictMode>
+//);
+
+// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
+
